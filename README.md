33af5c82c611aaf72ddd932838cde4ce4217757e
M Yakub Mizan <ctgcoder@gmail.com>
2015-08-22 08:06:41 +0300

    updated url

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
-# fast-mirror
-Find the fastest mirror
+# Fast Mirror
+
+A simple PHP script to find the fastest mirror based on ICMP ping.
+
+## Usage
+To echo the output
+
+```
+fast_mirrors('http://mirrors.ubuntu.com/mirrors.txt', true)
+
+```
+
+To return the output
+
+```
+fast_mirrors('http://mirrors.ubuntu.com/mirrors.txt', false)
+
+```
--- fast-mirror.php
+++ fast-mirror.php
@@ -1,46 +1,85 @@
 <?php
 
 /**
- * A simple script to "ping" a list of mirrors and return
- * the fastest. You can optionally update your
- * apt sources.list with the mirror
+ * Fast Mirror - find the fastest mirror with ICMP ping.
+ *
+ * A simple script to ping a list of mirrors and return
+ * the fastest.
+ *
+ * @author  M Yakub Mizan <mizan@shabdvisuals.com>
+ * @link    http://github.com/mymizan
  */
-function php_ping($host, $port, $timeout) {
-	$shell_output = shell_exec("ping -c1 -t{$timeout} {$host} | tail -1| awk '{print $4}' | cut -d '/' -f 2");
-	return $shell_output;
+
+/**
+ * Ping host using the Unix standard ping command
+ *
+ * @param  string  $host    hostname
+ * @param  integer $timeout timeout in seconds
+ * @return float            time in miliseconds
+ */
+function php_ping($host, $timeout) {
+	return shell_exec("ping -c1 -t{$timeout} {$host} | tail -1| awk '{print $4}' | cut -d '/' -f 2");
 }
 
-//http://mirrors.ubuntu.com/mirrors.txt
+/**
+ * Return a list of mirrors
+ *
+ * @param  string $mirror_url URL/file path containing url list
+ * @return array              return list of mirrors
+ */
 function mirror_list($mirror_url) {
 	$mirror_list = explode("\n", file_get_contents($mirror_url));
 	$mirrors = array();
 	if (count($mirror_list) > 0) {
 		foreach ($mirror_list as $key => $mirror) {
-			$mirrors[] = trim(parse_url($mirror)['host']);
+			$m = parse_url($mirror);
+			array_map('trim', $m); //trim whitespaces
+			$mirrors[] = $m;
 		}
 	}
 	return $mirrors;
 }
 
-$hosts = mirror_list("http://mirrors.ubuntu.com/mirrors.txt");
-$ping_time = array();
+/**
+ * Return the fastest mirror
+ * @param  string $mirror URL to a mirror list or file path
+ * @return string         return the url of the fastest mirror
+ */
+function fast_mirrors($mirror, $show = true) {
+	$hosts = mirror_list($mirror); //http://mirrors.ubuntu.com/mirrors.txt
+	$ping_time = array();
 
-echo "Total Hosts: " . count($hosts) . "\nPinging: ";
-foreach ($hosts as $key => $host) {
-	echo $key . ' |';
-	$time = trim(php_ping($host, '', 4));
-
-	if (intval($time) > 0) {
-		$ping_time[$time] = $host;
+	if ($show) {
+		echo "Total hosts to ping: " . count($hosts) . "\n";
+		echo "Pinging ... ";
 	}
 
-	if ($key > 0) {
-		break;
+	foreach ($hosts as $key => $host) {
+		if ($show) {
+			echo ($key + 1) . ' |';
+		}
+
+		$time = trim(php_ping($host['host'], 4));
+
+		if (intval($time) > 0) {
+			$ping_time[$time] = $host;
+		}
+		if ($key > 0) {
+			break;
+		}
+
 	}
+
+	ksort($ping_time); //short the keys
+
+	$fastest_mirror = array_keys($ping_time)[0];
+
+	if ($show) {
+		echo "\n";
+		echo "Fastest Mirror({$fastest_mirror}ms): " .
+		$ping_time[$fastest_mirror]['scheme'] .
+		'://' . $ping_time[$fastest_mirror]['host'] .
+		$ping_time[$fastest_mirror]['path'] . PHP_EOL;
+	}
+	return $ping_time[$fastest_mirror]['scheme'] . '://' . $ping_time[$fastest_mirror]['host'] . $ping_time[$fastest_mirror]['path'];
 }
-
-echo "\n";
-
-ksort($ping_time);
-$fastest_mirror = array_keys($ping_time)[0];
-echo "The Fastest Mirror: " . $ping_time[$fastest_mirror] . "({$fastest_mirror}ms)" . PHP_EOL;
